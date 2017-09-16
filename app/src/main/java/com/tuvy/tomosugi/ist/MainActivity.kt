a78571ccd16ddce65715911a9a14d5b863c22d54
Tomosugi Tasaka <tuvy.ano@gmail.com>
2017-09-16 17:54:33 +0900

    add WebSocket

--- app/src/main/java/com/tuvy/tomosugi/ist/MainActivity.kt
+++ app/src/main/java/com/tuvy/tomosugi/ist/MainActivity.kt
@@ -2,11 +2,31 @@ package com.tuvy.tomosugi.ist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.socket.client.IO
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("Main", "start")
+        val socket: Socket = IO.socket("http://153.126.157.25:5000")
+        socket
+                .on(Socket.EVENT_CONNECT, Emitter.Listener {
+                    Log.d("Socket", "Connect")
+                })
+                .on("event", Emitter.Listener {
+                    Log.d("Socket", "Event")
+                })
+                .on(Socket.EVENT_DISCONNECT, Emitter.Listener {
+                    Log.d("Socket", "Disconnect")
+                })
+        socket.connect()
+        Log.d("Main", "finish")
     }
 }
---
+++ app/src/main/java/com/tuvy/tomosugi/ist/controller/CoordinateClient.kt
@@ -0,0 +1,6 @@
+package com.tuvy.tomosugi.ist.controller
+
+/**
+ * Created by tomosugi on 2017/09/16.
+ */
+
